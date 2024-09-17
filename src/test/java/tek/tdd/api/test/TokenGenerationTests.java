750df1ef83a86cd4a49995443e9e7a529e08c2cb
Mohammad Shokriyan <mohammad@tekschool.us>
2024-09-16 20:21:33 -0400

    first test /api/token

--- src/test/java/tek/tdd/api/test/TokenGenerationTests.java
+++ src/test/java/tek/tdd/api/test/TokenGenerationTests.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import tek.tdd.base.ApiTestsBase;
 
@@ -14,12 +15,12 @@ public class TokenGenerationTests extends ApiTestsBase {
     private static final Logger LOGGER = LogManager.getLogger(TokenGenerationTests.class);
 
     //Create a test validate token generated with supervisor User
-    @Test
-    public void generateValidToken() {
+    @Test(dataProvider = "credentials")
+    public void generateValidToken(String username, String password) {
         RequestSpecification requestSpecification = getDefaultRequest();
         Map<String, String> body = new HashMap<>();
-        body.put("username", "supervisor");
-        body.put("password", "tek_supervisor");
+        body.put("username", username);
+        body.put("password", password);
 
         requestSpecification.body(body);
 
@@ -30,5 +31,16 @@ public class TokenGenerationTests extends ApiTestsBase {
         LOGGER.info("Response is {}", response.asPrettyString());
     }
 
+    @DataProvider(name = "credentials")
+    private String[][] credentials() {
+        return new String[][]{
+                {"supervisor", "tek_supervisor"},
+                {"operator_readonly", "Tek4u2024"},
+
+        };
+    }
+
     //Activity generate token with operator user
+//    username: operator_readonly
+//    password: Tek4u2024
 }
