6bf63ac116379f70438369095f78ed3e53b482c5
Mohammad Shokriyan <mohammad@tekschool.us>
2024-09-16 20:02:43 -0400

    first test /api/token

--- src/test/java/tek/tdd/base/ApiTestsBase.java
+++ src/test/java/tek/tdd/base/ApiTestsBase.java
@@ -1,12 +1,19 @@
 package tek.tdd.base;
 
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Listeners;
 
+@Listeners({ExtentITestListenerClassAdapter.class})
 public class ApiTestsBase extends BaseSetup {
+    private static final Logger LOGGER = LogManager.getLogger(ApiTestsBase.class);
 
     public RequestSpecification getDefaultRequest() {
+        LOGGER.info("Sending API call to {}" , RestAssured.baseURI);
         return RestAssured.given().contentType(ContentType.JSON);
     }
 }
