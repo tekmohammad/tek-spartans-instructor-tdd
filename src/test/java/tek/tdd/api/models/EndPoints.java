f83400c2822a7aa0e84cb8e1e1485077370b488c
Mohammad Shokriyan <mohammad@tekschool.us>
2024-09-17 21:07:29 -0400

    add get all plan code

--- src/test/java/tek/tdd/api/models/EndPoints.java
+++ src/test/java/tek/tdd/api/models/EndPoints.java
@@ -3,7 +3,8 @@ package tek.tdd.api.models;
 public enum EndPoints {
     TOKEN("/api/token"),
     GET_PRIMARY_ACCOUNT("/api/accounts/get-primary-account"),
-    GET_ACCOUNT("/api/accounts/get-account");
+    GET_ACCOUNT("/api/accounts/get-account"),
+    GET_ALL_PLAN_CODE("/api/plans/get-all-plan-code");
 
     private final String value;
 
---
+++ src/test/java/tek/tdd/api/test/PlanCodeTests.java
@@ -0,0 +1,48 @@
+package tek.tdd.api.test;
+
+import com.aventstack.extentreports.service.ExtentTestManager;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import tek.tdd.api.models.EndPoints;
+import tek.tdd.api.models.PlanCodeResponse;
+import tek.tdd.api.models.PlanCodesList;
+import tek.tdd.api.models.TokenRequest;
+import tek.tdd.base.ApiTestsBase;
+
+public class PlanCodeTests extends ApiTestsBase {
+    private static final Logger LOGGER = LogManager.getLogger(PlanCodeTests.class);
+
+    @Test
+    public void validatePlanCodes() {
+        TokenRequest tokenRequest = new TokenRequest("supervisor", "tek_supervisor");
+
+        String token = getDefaultRequest()
+                .body(tokenRequest)
+                .when()
+                .post(EndPoints.TOKEN.getValue())
+                .then().statusCode(200)
+                .extract()
+                .response()
+                .jsonPath().getString("token");
+
+        Response response = getDefaultRequest()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(EndPoints.GET_ALL_PLAN_CODE.getValue())
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        ExtentTestManager.getTest().info(response.asPrettyString());
+        LOGGER.info(response.asPrettyString());
+
+        PlanCodesList list = response.body().jsonPath().getObject("", PlanCodesList.class);
+
+        Assert.assertNotNull(list);
+    }
+}
